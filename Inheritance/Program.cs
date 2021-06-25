ef6db054b0f0b01cbb47da4f749ed5e3998c7831
AJeane <ajeane1924@gmail.com>
2021-06-24 22:38:22 -0500

    initial commit

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -28,12 +28,46 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird murkrow = new Bird()
+            {
+                Name = "Murkrow",
+                Age = 3,
+                Legs = 2,
+                HasBackbone = true,
+                IsWarmBlooded = true,
+                HasFeathers = true,
+                Wings = 2,
+                Type = "flying",
 
+            };
+
+            Console.WriteLine($"{murkrow.Name} is an {murkrow.Type} pokemon which is {murkrow.Age} years old and has it's normal number of {murkrow.Legs} legs and {murkrow.Wings} wings.");
+
+            Console.WriteLine($"If you are wondering if {murkrow.Name} has the 3 of these a bird should have, backbone, being warm blooded and having feathers, then it's {murkrow.HasBackbone}, {murkrow.IsWarmBlooded}, and {murkrow.HasFeathers}");
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+
+            Reptile gible = new Reptile()
+            {
+                Name = "Gible",
+                Age = 4,
+                Legs = 2,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                Environment = "geothermal areas",
+                Defense = "chomping and fighting",
+
+            };
+
+            Console.WriteLine($"{gible.Name} is an agressive pokemon that does {gible.Defense} also which is an average {gible.Age} years old and has a number of {gible.Legs} legs and lives in an {gible.Environment}.");
+
+            Console.WriteLine($"If you are wondering if {gible.Name} has the 3 of these a dragon type should have, backbone, being cold blooded and having scales, then it's {gible.HasBackbone}, {gible.IsColdBlooded}, and {gible.HasScales}");
+           
+
         }
     }
 }
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Reptile : Animal
+    {
+
+
+        public bool IsColdBlooded { get; set; }
+
+        public bool HasScales { get; set; }
+
+        public string Environment { get; set; }
+
+        public string Defense { get; set; }
+
+        public Reptile()
+        {
+
+        }
+
+        public Reptile(string name, int age, int legs, bool hasBackbone, bool isColdlooded, bool hasScales, string environment, string defense)
+        {
+            Name = name;
+            Age = age;
+            Legs = legs;
+            HasBackbone = hasBackbone;
+            IsColdBlooded = isColdlooded;
+            HasScales = hasScales;
+            Environment = environment;
+            Defense = defense;
+        }
+
+
+    }
+      
+}
